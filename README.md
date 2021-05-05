b6804e23113bd3a265e2978a816a8bda34f6a4b4
berserks03 <berserks@inbox.lv>
2021-05-05 12:45:25 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-In progress ...
+# Project
+
+Project consists of two parts - server-side and client-side. Each part should be run separately.
+
+## Client-side
+
+cd client
+
+### Install modules
+
+npm install
+
+### Start  application
+
+npm start
+
+View [http://localhost:8083](http://localhost:8083)
+
+## Server-side
+
+MySQL server has been set up locally using NodeJS. For testing setup your server with database matching project's configuration. Database name 'providerdb'.
+``CREATE TABLE `providers` (`id` INTEGER NOT NULL auto_increment , `provider` VARCHAR(255), `email` VARCHAR(255), `selected` TINYINT(1), `date` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;``
+
+cd server
+
+### Install modules
+
+npm install
+
+### Start  application
+
+npm start
+
+Servers run on ports 8082 and 8085
--- client/src/pages/api.tsx
+++ client/src/pages/api.tsx
@@ -1,14 +1,14 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import ProviderDataService from '../services/ProviderService';
-import Pagination from "@material-ui/lab/Pagination";
+import Pagination from '@material-ui/lab/Pagination';
 
-interface Provider  {
+interface Provider {
   id: number;
   email: string;
   provider: string;
   selected: boolean;
   date: Date;
-};
+}
 
 const convertTime = (time: Date) => {
   return new Date(time).toLocaleDateString().toString();
@@ -17,35 +17,35 @@ const convertTime = (time: Date) => {
 const Api = () => {
   const initialProviderState = {
     id: null,
-    provider: "",
-    email: "",
-    selected: false
+    provider: '',
+    email: '',
+    selected: false,
   };
   const [providers, setProviders] = useState<Provider[]>();
   const [filteredProviders, setFilteredProviders] = useState<Provider[]>();
   const [currentProvider, setCurrentProvider] = useState(initialProviderState);
   const [options, setOptions] = useState('date');
   const [inputValue, setInputValue] = useState('');
-  
+
   const [page, setPage] = useState(1);
   const [count, setCount] = useState(0);
   const [pageSize, setPageSize] = useState(10);
-  
+
   const pageSizes = [5, 10, 15];
 
   const ref: MutableRefObject<{ providers: string[] }> = useRef({
     providers: [],
   });
 
-  const getRequestParams = ( page: number, pageSize: number) => {
-    let params: {[key: string]: number} = {};
+  const getRequestParams = (page: number, pageSize: number) => {
+    let params: { [key: string]: number } = {};
 
     if (page) {
-      params["page"] = page - 1;
+      params['page'] = page - 1;
     }
 
     if (pageSize) {
-      params["size"] = pageSize;
+      params['size'] = pageSize;
     }
 
     return params;
@@ -53,7 +53,7 @@ const Api = () => {
 
   // fetching data
   const retrieveProviders = () => {
-    const params = getRequestParams( page, pageSize);
+    const params = getRequestParams(page, pageSize);
 
     ProviderDataService.getAll(params).then((response) => {
       const { providers, totalPages } = response.data;
@@ -69,7 +69,7 @@ const Api = () => {
           )
         );
     });
-  }
+  };
 
   useEffect(retrieveProviders, [page, pageSize]);
 
@@ -132,11 +132,16 @@ const Api = () => {
     return <div>No data to show</div>;
   }
 
-  const handlePageChange = (_event: React.ChangeEvent<unknown>, value:number) => {
+  const handlePageChange = (
+    _event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
     setPage(value);
   };
 
-  const handlePageSizeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+  const handlePageSizeChange = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
     setPageSize(event.target.value as number);
     setPage(1);
   };
@@ -154,39 +159,56 @@ const Api = () => {
 
   const getProvider = (id: number) => {
     ProviderDataService.get(id)
-      .then(response => {
+      .then((response) => {
         setCurrentProvider(response.data);
         console.log(response.data);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
+      retrieveProviders();
   };
 
-  const updateSelected = (status: boolean) => {
+  const updateSelected = (status: boolean) => {    
     const data = {
       id: currentProvider.id,
       provider: currentProvider.provider,
       email: currentProvider.email,
-      selected: status
+      selected: status,
     };
 
     ProviderDataService.update(currentProvider.id, data)
-      .then(response => {
+      .then((response) => {
         setCurrentProvider({ ...currentProvider, selected: status });
         console.log(response.data);
         retrieveProviders();
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   };
 
+  // const updateProvider = () => {
+  //   ProviderDataService.update(currentProvider.id, currentProvider)
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       retrieveProviders();
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
+  // };
+
   return (
     <section>
       <div>
-        <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}>
-
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+          }}
+        >
           <div>
             <h4>Filter by provider: </h4>
             {ref.current.providers.map((item) => {
@@ -212,11 +234,7 @@ const Api = () => {
               value={inputValue}
               onChange={(event) => setInputValue(event.target.value)}
             />
-            <button
-              className="ml-10"
-              type="button"
-              onClick={searchByEmail}
-            >
+            <button className='ml-10' type='button' onClick={searchByEmail}>
               Search
             </button>
           </div>
@@ -233,8 +251,15 @@ const Api = () => {
             </select>
           </div>
 
-          <div className="mt-20" style={{display: 'flex', flexBasis: '100%', justifyContent: 'center'}}>
-            {"Items per Page: "}
+          <div
+            className='mt-20'
+            style={{
+              display: 'flex',
+              flexBasis: '100%',
+              justifyContent: 'center',
+            }}
+          >
+            {'Items per Page: '}
             <select onChange={handlePageSizeChange} value={pageSize}>
               {pageSizes.map((size) => (
                 <option key={size} value={size}>
@@ -245,17 +270,24 @@ const Api = () => {
           </div>
 
           <Pagination
-            className="my-20"
+            className='my-20'
             count={count}
             page={page}
             siblingCount={1}
             boundaryCount={1}
-            variant="outlined"
-            shape="rounded"
+            variant='outlined'
+            shape='rounded'
             onChange={handlePageChange}
           />
-          
-          <div style={{display: 'flex', flexBasis: '100%', justifyContent: 'center', textAlign: 'center'}}>
+
+          <div
+            style={{
+              display: 'flex',
+              flexBasis: '100%',
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+          >
             <table>
               <thead>
                 <tr>
@@ -275,48 +307,76 @@ const Api = () => {
                     <td colSpan={6}>Database is empty</td>
                   </tr>
                 ) : (
-                  filteredProviders.map(({ id, email, provider, selected, date }, i) => {
-                    return (
-                      <tr key={id}>
-                        <td style={{ minWidth:'100px'}}>
-                          {selected? (
-                          <button type='button' onClick={()=>{getProvider(id);updateSelected(false)}}>
-                            unselect
-                          </button>
-                          ):(
-                            <button type='button' onClick={()=>{getProvider(id);updateSelected(true)}}>
-                            select
-                          </button>
-                          )}
-                        </td>
-                        <td style={{ minWidth:'50px'}}>{i + 1}</td>
-                        <td style={{ minWidth:'50px'}}>{id}</td>
-                        <td style={{ minWidth:'200px'}}>{email}</td>
-                        <td style={{ minWidth:'150px'}}>{provider}</td>
-                        <td style={{ minWidth:'100px'}}>{selected? "Yes" : "No"}</td>
-                        <td>{convertTime(date)}</td>
-                        <td style={{ minWidth:'100px'}}>
-                          <button type='button' onClick={() => deleteEmail(id)}>
-                            x
-                          </button>
-                        </td>
-                      </tr>
-                    );
-                  })
+                  filteredProviders.map(
+                    ({ id, email, provider, selected, date }, i) => {
+                      return (
+                        <tr key={id}>
+                          <td style={{ minWidth: '100px' }}>
+                            {selected ? (
+                              <button
+                                type='button'
+                                onClick={() => {
+                                  getProvider(id);
+                                  updateSelected(false);
+                                  // updateProvider();
+                                }}
+                              >
+                                unselect
+                              </button>
+                            ) : (
+                              <button
+                                type='button'
+                                onClick={() => {
+                                  getProvider(id);
+                                  updateSelected(true);
+                                  // updateProvider();
+                                }}
+                              >
+                                select
+                              </button>
+                            )}
+                          </td>
+                          <td style={{ minWidth: '50px' }}>{i + 1}</td>
+                          <td style={{ minWidth: '50px' }}>{id}</td>
+                          <td style={{ minWidth: '200px' }}>{email}</td>
+                          <td style={{ minWidth: '150px' }}>{provider}</td>
+                          <td style={{ minWidth: '100px' }}>
+                            {selected ? 'Yes' : 'No'}
+                          </td>
+                          <td>{convertTime(date)}</td>
+                          <td style={{ minWidth: '100px' }}>
+                            <button
+                              type='button'
+                              onClick={() => deleteEmail(id)}
+                            >
+                              x
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    }
+                  )
                 )}
               </tbody>
             </table>
           </div>
 
-          <button
-          className="m-20"
-          onClick={removeAllProviders}
-        >
-          Delete All
-        </button>
+          <button className='m-20' onClick={removeAllProviders}>
+            Delete All
+          </button>
 
-        <a href="http://localhost:8085/api/file" className="m-20" role="button" style={{display: 'flex', flexBasis: '100%', justifyContent: 'center'}} >Download CSV File</a>
-
+          <a
+            href='http://localhost:8085/api/file'
+            className='m-20'
+            role='button'
+            style={{
+              display: 'flex',
+              flexBasis: '100%',
+              justifyContent: 'center',
+            }}
+          >
+            Download CSV File
+          </a>
         </div>
       </div>
     </section>
